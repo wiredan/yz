be81db259cd851cac6675d8010ab61076cd2d8ad
wiredan <mtlhumaid@gmail.com>
2025-11-29 20:00:42 +0100

    Create src/components/footer.jsx

---
+++ src/components/footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+export default function Footer(){
+  return (
+    <footer style={{textAlign:'center', marginTop:24, color:'#aaa'}}>
+      Wiredan • Theme inspired by Binance • <a href="https://twitter.com/wilaya90">@wilaya90</a> • <a href="mailto:dansidran@gmail.com">dansidran@gmail.com</a>
+    </footer>
+  )
+}
