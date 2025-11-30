f14ef136193bfcb954944ec8bcbee7e6830787fc
wiredan <mtlhumaid@gmail.com>
2025-11-30 14:03:42 +0100

    Update app.jsx

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,10 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import OrderTracking from './components/OrderTracking';
+import AdminDashboard from './components/AdminDashboard';
+
 function App() {
   return (
-    <div>
-      <h1>Wiredan App</h1>
-      <p>Frontend is working!</p>
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/track-order" element={<OrderTracking />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+function Home() {
+  return (
+    <div>
+      <h1>Wiredan Marketplace</h1>
+      <nav>
+        <a href="/track-order">Track Your Order</a>
+        <a href="/admin">Admin Panel</a>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
